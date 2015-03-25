ff04f531b2b555cdbdbee6eb95f406cafc6d4e1b
Pierre <fritschpierre@gmail.com>
2015-03-25 08:28:48 +0100

    First tests

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -4,14 +4,47 @@
  * https://HosseinKarami.github.io/fastshell
  * @author Hossein Karami
  * @version 1.0.3
- * Copyright 2014. MIT licensed.
+ * Copyright 2015. MIT licensed.
  */
 (function ($, window, document, undefined) {
 
   'use strict';
 
-  $(function () {
-    // FastShell
+  $('.main-gallery').flickity({
+    // options
+    cellAlign: 'center',
+    wrapAround: true
   });
+
+
+  if (window.matchMedia('(min-width: 768px)').matches){
+
+    $(".hero-day").click(function(event) {
+      $(".hero-day").css('max-width', '');
+      $(this).removeClass('is-null');
+      $(this).toggleClass('is-full');
+    });
+    $( ".hero-night" ).click(function(event) {
+      $(".hero-day").css('max-width', '');
+      $(".hero-day").removeClass('is-full');
+      $(".hero-day").toggleClass('is-null');
+    });
+
+    $( ".hero" ).mousemove(function(event) {
+      var x = event.pageX,
+          containerWidth = $(this).outerWidth(),
+          posPercent = Math.floor((x * 100) / containerWidth),
+          isDay = $(".hero-day").hasClass('is-full'),
+          isNight = $(".hero-day").hasClass('is-null');
+      
+      if(isDay && posPercent > 90) {
+        $(".hero-day").css('max-width', '90%');
+      } else if(isNight && posPercent < 10) {
+        $(".hero-day").css('max-width', '10%');
+      } else {
+        $(".hero-day").css('max-width', '');
+      }
+    });
+  }
 
 })(jQuery, window, document);
