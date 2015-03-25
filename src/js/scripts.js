8d5ce3bb60e49121897a6928c3647ab4cae45b25
Pierre <fritschpierre@gmail.com>
2015-03-25 21:56:16 +0100

    Add grid system and reorder the css files

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,41 +2,44 @@
 
   'use strict';
 
-  $('.main-gallery').flickity({
-    // options
-    cellAlign: 'center',
-    wrapAround: true
-  });
-
-
-  if (window.matchMedia('(min-width: 768px)').matches){
-
-    $(".hero-day").click(function(event) {
-      $(".hero-day").css('max-width', '');
+  $('.hero-day').click(function() {
+    if (window.matchMedia('(min-width: 768px)').matches){
+      $('.hero-day').css('max-width', '');
       $(this).removeClass('is-null');
       $(this).toggleClass('is-full');
-    });
-    $( ".hero-night" ).click(function(event) {
-      $(".hero-day").css('max-width', '');
-      $(".hero-day").removeClass('is-full');
-      $(".hero-day").toggleClass('is-null');
-    });
+    } else {
+      $(this).removeClass('is-full');
+      $(this).toggleClass('is-null');
+    }
+  });
 
-    $( ".hero" ).mousemove(function(event) {
+  $( '.hero-night' ).click(function() {
+    if (window.matchMedia('(min-width: 768px)').matches){
+      $('.hero-day').css('max-width', '');
+      $('.hero-day').removeClass('is-full');
+      $('.hero-day').toggleClass('is-null');
+    } else {
+      $('.hero-day').removeClass('is-full');
+      $('.hero-day').toggleClass('is-null');
+    }
+  });
+
+  $( '.hero' ).mousemove(function() {
+    if (window.matchMedia('(min-width: 768px)').matches){
       var x = event.pageX,
           containerWidth = $(this).outerWidth(),
           posPercent = Math.floor((x * 100) / containerWidth),
-          isDay = $(".hero-day").hasClass('is-full'),
-          isNight = $(".hero-day").hasClass('is-null');
+          isDay = $('.hero-day').hasClass('is-full'),
+          isNight = $('.hero-day').hasClass('is-null');
       
       if(isDay && posPercent > 90) {
-        $(".hero-day").css('max-width', '90%');
+        $('.hero-day').css('max-width', '90%');
       } else if(isNight && posPercent < 10) {
-        $(".hero-day").css('max-width', '10%');
+        $('.hero-day').css('max-width', '10%');
       } else {
-        $(".hero-day").css('max-width', '');
+        $('.hero-day').css('max-width', '');
       }
-    });
-  }
+    }
+  });
 
 })(jQuery, window, document);
