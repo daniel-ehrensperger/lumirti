1a348b369d69dbcb5b9969546b4b814f40f8fd63
Pierre <fritschpierre@gmail.com>
2015-03-28 14:28:57 +0100

    Add a simple contact form

---
+++ app/contact_me.php
@@ -0,0 +1,60 @@
+<?php
+if($_POST) {
+  $toEmail = "fritschpierre@gmail.com"; //Recipient email, Replace with own email here
+
+  //check if its an ajax request, exit if not
+  if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    $output = json_encode(array( //create JSON data
+      'type'=>'error', 
+      'text' => 'Sorry Request must be Ajax POST'
+    ));
+    die($output); //exit script outputting json data
+  } 
+  
+  //Sanitize input data using PHP filter_var().
+  $userName    = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+  $userEmail   = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $phoneNumber = filter_var($_POST["phone"], FILTER_SANITIZE_NUMBER_INT);
+  $message    = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+  $subject = "Message form ".$userName." via lumitri.com";
+  
+  //additional php validation
+  $valid = true;
+  $msg = '';
+  if(strlen($userName)<3){ // If length is less than 3 it will output JSON error.
+    $msg .= 'Really? Is that your name?<br>';
+    $valid = false;
+  }
+  if(!filter_var($userEmail, FILTER_VALIDATE_EMAIL)){ //email validation
+    $msg .= 'Are you sure about your email address?<br>';
+    $valid = false;
+  }
+  if(strlen($message)<3){ //check emtpy message
+    $msg .= 'Mhh, I\'m not sure to understand your message...';
+    $valid = false;
+  }
+  if (!$valid) {
+    $output = json_encode(array('type'=>'error', 'text' => $msg));
+    die($output);
+  }
+  
+  //email body
+  $message_body = $message."\r\n\r\n-".$userName."\r\nEmail : ".$userEmail."\r\nPhone Number : ". $phoneNumber ;
+  
+  //proceed with PHP email.
+  $headers = 'From: '.$userEmail.'' . "\r\n" .
+  'Reply-To: '.$userEmail.'' . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+  
+  $send_mail = mail($toEmail, $subject, $message_body, $headers);
+  
+  if(!$send_mail) {
+    //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
+    $output .= json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+    die($output);
+  } else {
+    $output .= json_encode(array('type'=>'message', 'text' => 'Thank you for your message '.$userName .'!'));
+    die($output);
+  }
+}
+?>
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -27,10 +27,10 @@
   $( '.hero' ).mousemove(function() {
     if (window.matchMedia('(min-width: 768px)').matches){
       var x = event.pageX,
-          containerWidth = $(this).outerWidth(),
-          posPercent = Math.floor((x * 100) / containerWidth),
-          isDay = $('.hero-day').hasClass('is-full'),
-          isNight = $('.hero-day').hasClass('is-null');
+      containerWidth = $(this).outerWidth(),
+      posPercent = Math.floor((x * 100) / containerWidth),
+      isDay = $('.hero-day').hasClass('is-full'),
+      isNight = $('.hero-day').hasClass('is-null');
       
       if(isDay && posPercent > 90) {
         $('.hero-day').css('max-width', '90%');
@@ -40,6 +40,57 @@
         $('.hero-day').css('max-width', '');
       }
     }
+  });
+
+  // Contact form
+  $('#submit-btn').click(function() { 
+
+    var proceed = true;
+    //simple validation at client's end
+    //loop through each field and we simply change border color to red for invalid fields   
+    $('.contact-input input[required=true], .contact-input textarea[required=true]').each(function(){
+      $(this).removeClass('is-empty'); 
+      if(!$.trim($(this).val())){ //if this field is empty 
+        $(this).addClass('is-empty'); //change border color to red   
+        // proceed = false; //set do not proceed flag
+      }
+      //check invalid email
+      var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+      if($(this).attr('type')==='email' && !emailReg.test($.trim($(this).val()))){
+        $(this).addClass('is-empty');
+        // proceed = false; //set do not proceed flag        
+      } 
+    });
+
+    if(proceed){ //everything looks good! proceed...
+      //get input field values data to be sent to server
+      var postData = {
+        'name'   : $('input[name=name]').val(), 
+        'email'  : $('input[name=email]').val(), 
+        'phone'  : $('input[name=phone]').val(),
+        'message': $('textarea[name=message]').val()
+      };
+
+      // Ajax post data to server
+      $.post('contact_me.php', postData, function(response){  
+        var output = '';
+        if(response.type === 'error'){ //load json data from server and output message     
+          output = '<span class="is-error">'+response.text+'</span>';
+        } else {
+          output = '<span class="is-success">'+response.text+'</span>';
+          //reset values in all input fields
+          $('.contact-input input, .contact-input textarea').val(''); 
+          // $('.contact-body').slideUp(); //hide form after success
+        }
+        $('.contact-result').hide().html(output).slideDown();
+      }, 'json');
+    } 
+  });
+
+  //reset previously set border colors and hide all message on .keyup()
+  $('.contact-input  input, .contact-input textarea').keyup(function() { 
+    $(this).removeClass('is-empty'); 
+    $('.contact-result').slideUp();
   });
 
 })(jQuery, window, document);
